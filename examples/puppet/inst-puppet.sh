df65c6247477e7db65c515df4821ea1c36258cc6
Mika Eloranta <mika.eloranta@gmail.com>
2010-11-30 08:52:15 +0200

    tuned puppet example to make version control checkpoints

--- examples/puppet/inst-puppet.sh
+++ examples/puppet/inst-puppet.sh
@@ -9,15 +9,16 @@ rm -rf $REPO
 
 poni -d $REPO script -v <<EOF
 init
+vc init
 
-add-node template/ec2-deb6
-add-config template/ec2-deb6 hacks
+add-config -cd ec2-deb6/ template/ec2-deb6 hacks
 set template\$ verify=bool:false
+vc commit "added templates"
 
-add-node software
+add-config -cd puppet-master/ software puppet-master-v1.0
+add-config -cd puppet-agent/ software puppet-agent-v1.0
 set software\$ verify=bool:false
-add-config software puppet-master-v1.0
-add-config software puppet-agent-v1.0
+vc commit "added software"
 
 add-node puppet/master -i template/ec2-deb6
 add-config puppet/master puppet-master -i software/puppet-master-v1.0
@@ -26,12 +27,9 @@ set puppet/master cloud.provider=aws-ec2 cloud.region=us-east-1 cloud.image=ami-
 add-node nodes/demo/server{id:02} -c2 -i template/ec2-deb6
 add-config nodes/demo/server puppet-agent -i software/puppet-agent-v1.0
 set nodes/demo/server cloud.provider=aws-ec2 cloud.region=us-east-1 cloud.image=ami-daf615b3 cloud.kernel=aki-6eaa4907 cloud.ramdisk=ari-42b95a2b cloud.type=m1.small cloud.key-pair=$AWS_KEYPAIR user=root
+vc commit "added nodes"
 
 EOF
-
-cp ec2-deb6/* $REPO/system/template/ec2-deb6/config/hacks/
-cp puppet-master/* $REPO/system/software/config/puppet-master-v1.0/
-cp puppet-agent/* $REPO/system/software/config/puppet-agent-v1.0/
 
 # NOTE: verify cannot be run until hardware has been provisioned and
 #       network addresses have been updated
