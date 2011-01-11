49686590f05dcf1c9b4db8a06793b09996016519
Mika Eloranta <mika.eloranta@gmail.com>
2011-01-11 15:42:19 +0200

    require GitPython>=0.3.1 and Cheetah>=2.4.2

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ long_desc = file("%s/README.rst" % new_dir).read()
 depends = [
     "path.py>=2.2.2",
     "paramiko>=1.7.6",
-    "cheetah",
-    "GitPython",
+    "cheetah>=2.4.2",
+    "GitPython>=0.3.1",
     "argh>=0.13",
     ]
 
