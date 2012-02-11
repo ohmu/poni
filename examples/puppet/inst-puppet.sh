131255ff9bd53c8d3096d73663e28b5f928d26dd
Mika Eloranta <mika.eloranta@gmail.com>
2012-02-11 14:11:40 +0200

    examples: fixed puppet example inst-puppet.sh script

--- examples/puppet/inst-puppet.sh
+++ examples/puppet/inst-puppet.sh
@@ -14,12 +14,12 @@ vc init
 
 add-config -cd ec2-deb6/ template/ec2-deb6 hacks
 set -M template template:bool=true
--T vc checkpoint "added templates"
+vc checkpoint "added templates"
 
 add-config -cd puppet-master/ software puppet-master-v1.0
 add-config -cd puppet-agent/ software puppet-agent-v1.0
 set -M software template:bool=true
--T vc checkpoint "added software"
+vc checkpoint "added software"
 
 add-node example/master -i template/ec2-deb6
 add-config example/master puppet-master -i software/puppet-master-v1.0
@@ -28,6 +28,6 @@ set example/master cloud.provider=aws-ec2 cloud.region=us-east-1 cloud.image=ami
 add-node example/demo/server{id:02} -n2 -i template/ec2-deb6
 add-config example/demo/server puppet-agent -i software/puppet-agent-v1.0
 set example/demo/server cloud.provider=aws-ec2 cloud.region=us-east-1 cloud.image=ami-daf615b3 cloud.kernel=aki-6eaa4907 cloud.ramdisk=ari-42b95a2b cloud.type=m1.small cloud.key-pair=$AWS_KEYPAIR user=root
--T vc checkpoint "added nodes"
+vc checkpoint "added nodes"
 report
 EOF
