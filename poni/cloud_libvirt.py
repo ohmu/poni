c37c1f7ffe0ec326f6037feeed9d736cb4f8038a
Oskari Saarenmaa <os@ohmu.fi>
2015-02-16 12:49:02 +0200

    cloud_libvirt: refactored address discovery using IPv6 tunnels
    * Proper support for link-local addresses
    * Seprate address discovery function and the actual methods it uses
      in another function to allow calling it outside wait_instances

--- poni/cloud_libvirt.py
+++ poni/cloud_libvirt.py
@@ -168,24 +168,6 @@ def ignore_libvirt_errors(*errs):
     return decorate
 
 
-def parse_ip_addr(output, macs, deploy_if_name=None):
-    """Parse addresses from 'ip addr' output"""
-    # 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
-    #     link/ether 52:54:00:a0:b9:b6 brd ff:ff:ff:ff:ff:ff
-    #     inet 10.133.58.56/20 brd 10.133.63.255 scope global eth0
-    priv_ipv4 = None
-    deploy_addr = None
-    addrs = re.findall("^[0-9]+: ([a-z0-9]+):.+\n +link/ether ([0-9a-f:]+).*\n +inet ([0-9.]+)/", output, re.MULTILINE)
-    for name, mac, ipv4 in addrs:
-        if not priv_ipv4 and mac == macs[0]:
-            priv_ipv4 = ipv4  # the first interface is always the one used as the 'private.ip'
-
-        if name == deploy_if_name:
-            deploy_addr = ipv4
-
-    return deploy_addr, priv_ipv4
-
-
 class LibvirtProvider(Provider):
     def __init__(self, cloud_prop):
         if MISSING_LIBS:
@@ -404,6 +386,7 @@ class LibvirtProvider(Provider):
             instance["ipproto"] = prop.get("ipproto", "ipv4")
             instance["macs"] = vm.macs
             instance["ipv6"] = vm.ipv6_addr(ipv6pre)[0]
+            instance["deploy_if"] = prop.get("deploy_if")
             instance["ssh_key"] = "{0}/.ssh/{1}".format(home, prop["ssh_key"])
             instances.append(instance)
 
@@ -424,100 +407,136 @@ class LibvirtProvider(Provider):
         tasks.wait_all()
         boot_started = time.time()
 
-        # get ipv4 addresses for the hosts (XXX: come up with something better)
         result = {}
-        tunnels = {}
-        failed = []
-        objs = []
-        timeout = 120
-        start = time.time()
+        for vm_name, addr in self.discover_instances(instances):
+            result[vm_name] = {
+                "host": addr["deploy_addr"] or addr["first_addr"],
+                "private": {"ip": addr["first_addr"], "dns": addr["first_addr"]},
+                "hypervisor": self.hypervisor,
+                }
 
-        for attempt in xrange(1, 1000):
-            elapsed = time.time() - start
-            if elapsed > timeout:
-                raise LVPError("Connecting to {0!r} failed".format(failed))
-            if attempt > 1:
-                time.sleep(2)
-            self.log.info("getting ip addresses: round #%r, time spent=%.02fs", attempt, elapsed)
-            failed = []
-
-            for instance in instances:
-                instance_id = instance["vm_name"]
-                if instance["ipproto"] in instance:
-                    # address already exists (ie lookup done or we're using ipv6)
-                    if instance_id not in result:
-                        addr = instance[instance['ipproto']]
-                        result[instance_id] = dict(host=addr, private=dict(ip=addr, dns=addr))
-                    continue
-
-                conn = instance["vm_conns"][0]
-                if conn not in tunnels:
-                    client = paramiko.SSHClient()
-                    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    client.connect(conn.host, port=conn.port, username=conn.username, key_filename=conn.keyfile)
-                    tunnels[conn] = client
-                trans = tunnels[conn].get_transport()
-
-                ipv4 = None
-                try:
-                    tunchan = trans.open_channel("direct-tcpip", (instance["ipv6"], 22), ("localhost", 0))
-                    client = TunnelingSSHClient()
-                    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    client.connect(instance["ipv6"], sock=tunchan,
-                                   username="root", key_filename=instance["ssh_key"])
-                    cmdchan = client.get_transport().open_session()
-                    cmdchan.set_combine_stderr(True)
-                    cmdchan.exec_command('ip addr show scope global')
-                    cmdchan.shutdown_write()
-                    exec_start = time.time()
-                    while (not cmdchan.exit_status_ready()) and ((time.time() - exec_start) < 10.0):
-                        time.sleep(0.05)
-
-                    if cmdchan.exit_status_ready():
-                        exit_code = cmdchan.recv_exit_status()
-                        if exit_code != 0:
-                            self.log.warning("remote command non-zero exit status: exitcode=%s, %r", exit_code, instance)
-
-                    data = cmdchan.recv(1024)
-                    objs.extend((tunchan, cmdchan, client))
-                except (socket.error, socket.gaierror, paramiko.SSHException) as ex:
-                    self.log.warning("connecting to %r [%s] failed: %r", instance, instance["ipv6"], ex)
-                else:
-                    if data:
-                        deploy_if = instance["prop"].get("deploy_if")
-                        deploy_addr, ipv4 = parse_ip_addr(data, instance["macs"], deploy_if_name=deploy_if)
-                        if not ipv4:
-                            self.log.warning("no ipv4 yet available from: %s", instance)
-                        elif deploy_if and not deploy_addr:
-                            self.log.warning("no deploy address (%s) yet available for: %s", deploy_if, instance)
-                            ipv4 = None
-                    else:
-                        self.log.warning("no data received from: %r", instance)
-
-                if not ipv4:
-                    failed.append(instance)
-                else:
-                    deploy_addr_str = " (deploy address: {0})".format(deploy_addr) if deploy_addr else ""
-                    self.log.info("Got address %r for %s%s", ipv4, instance["vm_name"], deploy_addr_str)
-                    instance['ipv4'] = ipv4
-                    addr = instance[instance['ipproto']]
-                    result[instance_id] = dict(
-                        host=deploy_addr or ipv4,
-                        private=dict(ip=addr, dns=addr),
-                        hypervisor=self.hypervisor)
-
-            if not failed:
-                break
-
-        self.log.info("instances ready: delete {1:.2f}s, cloning {2:.2f}s, boot {0:.2f}s".format(
-            cloning_started - delete_started, boot_started - cloning_started,
-            time.time() - boot_started))
-
-        for client in tunnels.itervalues():
-            client.close()
+        self.log.info("instances ready: delete %.2fs, cloning %.2fs, discovery %.2fs",
+                      cloning_started - delete_started,
+                      boot_started - cloning_started,
+                      time.time() - boot_started)
 
         self.disconnect()
         return result
+
+    def discover_instances(self, instances):
+        # find public addresses for instances using the configured method
+        context = {"cleanup": []}  # method specific context for persisting connections, etc
+        failed = []
+        timeout = 120
+        start = time.time()
+        missing = set(instance["vm_name"] for instance in instances)
+        attempt = 0
+
+        while missing:
+            attempt += 1
+            elapsed = time.time() - start
+            if elapsed > timeout:
+                raise LVPError("Connecting to {0!r} failed".format(missing))
+            if attempt > 1:
+                time.sleep(2)
+            self.log.info("getting ip addresses: round #%r, time spent=%.02fs", attempt, elapsed)
+
+            for instance in instances:
+                vm_name = instance["vm_name"]
+                if vm_name not in missing:
+                    continue
+
+                addrs = self._get_instance_address_ipv6autoconf(instance, context)
+                if not addrs:
+                    self.log.warning("no ip addresses yet for: %s", instance)
+                    continue
+
+                # look up the 'ipproto' type address for the first interface
+                # and the address of the deployment interface (if defined)
+                deploy_if = instance.get("deploy_if")
+                first_addr = None
+                deploy_addr = None
+                for iface in addrs:
+                    # the first interface is always the one used as the 'private.ip'
+                    if not first_addr and iface["hardware-address"] == instance["macs"][0]:
+                        for addr in iface["ip-addresses"]:
+                            if addr["ip-address-type"] == instance["ipproto"]:
+                                first_addr = addr["ip-address"]
+                                break
+                    # find the address for the deployment interface
+                    if iface["name"] == deploy_if:
+                        for addr in iface["ip-addresses"]:
+                            if addr["ip-address-type"] == instance["ipproto"]:
+                                deploy_addr = addr["ip-address"]
+                                break
+
+                if not first_addr:
+                    self.log.warning("no private address yet for: %s", instance)
+                elif deploy_if and not deploy_addr:
+                    self.log.warning("no deploy interface address (%s) yet for: %s", deploy_if, instance)
+                else:
+                    missing.remove(vm_name)
+                    instance[instance['ipproto']] = first_addr
+                    deploy_addr_str = " (deploy address: {0})".format(deploy_addr) if deploy_addr else ""
+                    self.log.info("Got address %r for %s%s", first_addr, instance["vm_name"], deploy_addr_str)
+                    yield (vm_name, {"deploy_addr": deploy_addr, "first_addr": first_addr})
+
+        for cleanup_func in context["cleanup"]:
+            cleanup_func()
+
+    def _get_instance_address_ipv6autoconf(self, instance, context):
+        """find public addresses for instances based on ipv6 autoconfiguration
+           by opening ssh connection from local host to hypervisor and an ipv6
+           tunnel using link-local address through it to the target host"""
+        if 'ipv6autoconf' not in context:
+            context['ipv6autoconf'] = True
+            context['tunnels'] = {}  # connections to hypervisors
+            context['objs'] = []  # references for paramiko objects
+            def cleanup():
+                for client in context['tunnels'].values():
+                    client.close()
+            context['cleanup'].append(cleanup)
+
+        conn = instance["vm_conns"][0]
+        if conn not in context['tunnels']:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(conn.host, port=conn.port, username=conn.username, key_filename=conn.keyfile)
+            context['tunnels'][conn] = client
+        trans = context['tunnels'][conn].get_transport()
+        addr = instance["ipv6"]
+
+        try:
+            tunchan = trans.open_channel("direct-tcpip", (addr, 22), ("localhost", 0))
+            client = TunnelingSSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(addr, sock=tunchan,
+                           username=instance.get("username", "root"),
+                           key_filename=instance["ssh_key"])
+            cmdchan = client.get_transport().open_session()
+            cmdchan.set_combine_stderr(True)
+            cmdchan.exec_command('PATH=/sbin:/usr/sbin:$PATH ip addr show scope global')
+            cmdchan.shutdown_write()
+            exec_start = time.time()
+            while (not cmdchan.exit_status_ready()) and ((time.time() - exec_start) < 10.0):
+                time.sleep(0.05)
+
+            if cmdchan.exit_status_ready():
+                exit_code = cmdchan.recv_exit_status()
+                if exit_code != 0:
+                    self.log.warning("remote command non-zero exit status: exitcode=%s, %r", exit_code, instance)
+
+            data = cmdchan.recv(1024)
+            context['objs'].extend((tunchan, cmdchan, client))
+        except (socket.error, paramiko.SSHException) as ex:
+            self.log.warning("connecting to %r [%s] failed: %r", instance, addr, ex)
+            return None
+
+        if not data:
+            self.log.warning("no data received from: %r", instance)
+            return None
+
+        return list(parse_ip_addr(data))
 
     def power_on_instances(self, props):
         result = self._vm_async_apply(props, 'power_on')
@@ -589,6 +608,7 @@ class PoniLVConn(object):
         self.conn = None
         self.node = None
         self.info = None
+        self.networks = None
         self.dominfo = None
         self._dominfo_lock = threading.Lock()
 
@@ -613,8 +633,8 @@ class PoniLVConn(object):
             self.emulator = caps.find("guest").find("arch").find("emulator").text
 
     def refresh(self):
-        self.refresh_list()
         self.refresh_node()
+        self.refresh_list()
 
     def refresh_node(self):
         assert self.conn, "not connected"
@@ -624,6 +644,8 @@ class PoniLVConn(object):
         node["total_mhz"] = node["sockets"] * node["cores"] * node["mhz"]
         self.node = node
         self.info = node
+        self.networks = dict((name, self.conn.networkLookupByName(name).bridgeName())
+                             for name in self.conn.listNetworks())
 
     def refresh_list(self):
         """Refresh the domain lists unless someone else is already doing the refresh"""
@@ -981,13 +1003,17 @@ class PoniLVDom(object):
         self.dom = dom
         self.name = dom.name()
         self.macs = []
+        self.bridges = []
         self.disks = []
         self.fss = []
         self.info = {}
         self.__dom_info()
         self.__read_desc()
 
-    def ipv6_addr(self, prefix="fe80::"):
+    def ipv6_addr(self, prefix=None):
+        if prefix is None or prefix == "fe80::":
+            return ["{0}%{1}".format(mac_to_ipv6("fe80::", mac), self.bridges[i])
+                    for i, mac in enumerate(self.macs)]
         return [mac_to_ipv6(prefix, mac) for mac in self.macs]
 
     @convert_libvirt_errors
@@ -1026,8 +1052,21 @@ class PoniLVDom(object):
         xml = etree.fromstring(self.dom.XMLDesc(0))
         devs = xml.find("devices")
         if devs is not None:
-            self.macs = [str(iface.find("mac").get("address"))
-                         for iface in devs.iter("interface")]
+            self.macs = []
+            self.bridges = []
+            for iface in devs.iter("interface"):
+                self.macs.append(str(iface.find("mac").get("address")))
+                source = iface.find("source")
+                # look up the host bridge this device is attached to, we
+                # need it to connect to the link-local ipv6 address
+                bridge = source.get("bridge")
+                if bridge is None:
+                    network = source.get("network")
+                    if network is not None:
+                        bridge = self.conn.networks.get(str(network))
+                else:
+                    bridge = str(bridge)
+                self.bridges.append(bridge)
             self.disks = [str(disk.find("source").get("file") or disk.find("source").get("dev"))
                           for disk in devs.iter("disk")]
             self.fss = [str(fs.find("source").get("dir"))
@@ -1072,6 +1111,36 @@ class PoniLVDom(object):
         self.log.info("reverting %r to %r by force on %r", name, self.name, self.conn.host)
         snap = self.dom.snapshotLookupByName(name, 0)
         self.dom.revertToSnapshot(snap, libvirt.VIR_DOMAIN_SNAPSHOT_REVERT_FORCE)
+
+
+def parse_ip_addr(data):
+    """Parse addresses from 'ip addr' output"""
+    # 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
+    #     link/ether 52:54:00:a0:b9:b6 brd ff:ff:ff:ff:ff:ff
+    #     inet 10.133.58.56/20 brd 10.133.63.255 scope global eth0
+    #        valid_lft 85266sec preferred_lft 85266sec
+    #     inet6 2001:1bc8:100:6::f301/64 scope global
+    #        valid_lft forever preferred_lft forever
+    for iface in re.split("^[0-9]+: ", data.strip(), flags=re.MULTILINE):
+        if not iface:
+            continue
+        lines = [l.strip() for l in iface.splitlines()]
+        info = {
+            "name": lines.pop(0).partition(":")[0],
+            "ip-addresses": [],
+            "hardware-address": None,
+            }
+        for line in lines:
+            words = line.split()
+            if words[0].startswith("link/") and len(words) >= 2:
+                info["hardware-address"] = words[1]
+            elif words[0] in ("inet", "inet6"):
+                addrtype = "ipv6" if words[0] == "inet6" else "ipv4"
+                addr, _, prefix = words[1].partition("/")
+                if prefix == '':
+                    prefix = 128 if addrtype == "ipv6" else 32
+                info["ip-addresses"].append({"ip-address-type": addrtype, "ip-address": addr, "prefix": int(prefix)})
+        yield info
 
 
 def mac_to_ipv6(prefix, mac):
